685ce4b4eebe3fbf1c5db0f4a6a413798693a70e
Eric Knight <425302118@qq.com>
2023-01-27 11:18:25 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import "./styles.css";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+import PlayList from "./components/PlayList";
+
+export default function App() {
+  const [name, setName] = useState("");
+  const [musicList, setMusicList] = useState([]);
+  const [state, setState] = useState("idle");
+
+  useEffect(() => {
+    // 获取精品歌单
+    const fetchData = () => {
+      return axios
+        .get("http://localhost:3000/top/playlist/highquality")
+        .then((response) => {
+          const { data } = response;
+          setMusicList(data.playlists);
+        });
+    };
+    fetchData().catch((error) => {
+      setState("error: " + error.message);
+    });
+  }, []);
+
+  return (
+    <div className="App container text-light">
+      <div className="container text-center">
+        <h1>Cloud Music</h1>
+        <h2>发现精品歌单</h2>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          className="my-3"
+        />
+        {name && <p>Your are searching: {name}</p>}
+      </div>
+      <div className="container">
+        <PlayList musicList={musicList} state={state} />
+      </div>
+    </div>
+  );
+}
---
+++ src/components/PlayList.js
@@ -0,0 +1,24 @@
+import PlayListItem from "./PlayListItem";
+import PropTypes from "prop-types";
+
+export default function PlayList({ musicList, state }) {
+  return (
+    <ul className="row">
+      {musicList.length &&
+        musicList.map((item) => (
+          <li
+            key={item.id}
+            className="col-xl-3 col-lg-4 col-md-6 col-sm-12 col-xs-12 my-2"
+          >
+            <PlayListItem item={item} />
+          </li>
+        ))}
+      {!musicList.length && <p>{state}</p>}
+    </ul>
+  );
+}
+
+PlayList.propTypes = {
+  musicList: PropTypes.array,
+  state: PropTypes.string.isRequired
+};
